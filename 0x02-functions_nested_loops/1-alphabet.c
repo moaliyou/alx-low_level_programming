11f2242c33956adb3d84a867c374bf90025a47c9
moaliyou <moaliyou47@gmail.com>
2022-12-13 11:46:55 -0800

    a function that prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet()
+void print_alphabet(void)
 {
 	int ch;
 
@@ -16,5 +16,4 @@ void print_alphabet()
 		ch++;
 	}
 	_putchar('\n');
-	return;
 }
